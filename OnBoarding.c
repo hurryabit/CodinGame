60d070f2a87f30d2ed939c0e1a79fc05db4f7f9c
Martin Huschenbett <martin.huschenbett@posteo.de>
2015-04-27 13:14:31 +0200

    Solved "On Boarding" in C.

---
+++ OnBoarding.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  while( 1 ) {
+    char enemy1[256];
+    scanf("%s", enemy1);
+    int dist1;
+    scanf("%d", &dist1);
+    char enemy2[256];
+    scanf("%s", enemy2);
+    int dist2;
+    scanf("%d", &dist2);
+
+    printf("%s\n", dist1 < dist2 ? enemy1 : enemy2);
+  }
+}
