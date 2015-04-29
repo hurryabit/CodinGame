253d12d36c3747989216789b33e3ac1067004772
Martin Huschenbett <martin.huschenbett@posteo.de>
2015-04-29 18:51:20 +0200

    PoT292.

--- PowerOfThorMinimal.hs
+++ PowerOfThorMinimal.hs
@@ -1,6 +1,6 @@
 import System.IO
 main=hSetBuffering stdout NoBuffering>>getLine>>=(\[k,l,s,t]->let
-  z=cycle[0,1,1,1,0,-1,-1,-1]
-  loop(x,y)=putStrLn(words"N NE E SE S SW W NW"!!d)>>loop q
-   where (_,q,d)=minimum[(abs(k-u)+abs(l-v),p,i)|i<-[0..7],let p@(u,v)=(x+z!!i,y+z!!(i+6))]
+  z=[0,1,1,1,0,-1,-1,-1]
+  loop(x,y)=putStrLn(words"N NE E SE S SW W NW"!!d)>>loop p
+   where (_,p,d)=minimum[(abs(k-u)+abs(l-v),(u,v),i)|i<-[0..7],let u=x+z!!i;v=y+z!!mod(i+6)8]
  in loop(s,t)).map read.words
