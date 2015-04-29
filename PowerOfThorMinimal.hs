442aada5be4668369c4ee1b60ae555f96d17094b
Martin Huschenbett <martin.huschenbett@posteo.de>
2015-04-29 20:34:32 +0200

    PoT220.

--- PowerOfThorMinimal.hs
+++ PowerOfThorMinimal.hs
@@ -1,7 +1,7 @@
-import System.IO
-main=hSetBuffering stdout NoBuffering>>getLine>>=(\[a,b,s,t]->let i[a,b]x|x>0=a|True=b;x=s-a;y=t-b;u=abs x;v=abs y;d=[i"NS"y,i"WE"x] in mapM_ putStrLn$replicate(min u v)d++repeat[i d(v-u)]).map read.words
+main=getLine>>=(\[a,b,s,t]->let i[a,b]x|x>0=a|True=b;x=s-a;y=t-b;u=abs x;v=abs y;d=[i"NS"y,i"WE"x]in mapM putStrLn$replicate(min u v)d++repeat[i d(v-u)]).map read.words
 
- -- main=hSetBuffering stdout NoBuffering>>getLine>>=(\[a,b,s,t]->
+
+ -- main=getLine>>=(\[a,b,s,t]->
  --  let
  --   z=[0,1,1,1,0,-1,-1,-1]
  --   l(x,y)=
