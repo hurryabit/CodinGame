c59ec654d54c16059a2e9d384ded14e217c43201
Martin Huschenbett <martin.huschenbett@posteo.de>
2015-04-29 18:47:02 +0200

    Below 300 in minimal "PoT".

--- PowerOfThorMinimal.hs
+++ PowerOfThorMinimal.hs
@@ -1,8 +1,6 @@
 import System.IO
-import Data.List
-import Data.Ord
 main=hSetBuffering stdout NoBuffering>>getLine>>=(\[k,l,s,t]->let
   z=cycle[0,1,1,1,0,-1,-1,-1]
-  loop(x,y)=putStrLn(words"N NE E SE S SW W NW"!!d)>>loop(m d)
-   where (m,d)=(\i->(x+z!!i,y+z!!(i+6)),minimumBy(comparing$(\(u,v)->abs(k-u)+abs(l-v)).m)[0..7])
+  loop(x,y)=putStrLn(words"N NE E SE S SW W NW"!!d)>>loop q
+   where (_,q,d)=minimum[(abs(k-u)+abs(l-v),p,i)|i<-[0..7],let p@(u,v)=(x+z!!i,y+z!!(i+6))]
  in loop(s,t)).map read.words
