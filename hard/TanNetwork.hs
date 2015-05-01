276f8dfb1be91c8b78249cfe2387a7b3b299cc7d
Martin Huschenbett <martin.huschenbett@posteo.de>
2015-05-01 20:35:37 +0200

    Next failed attempt to "TAN Network".

--- hard/TanNetwork.hs
+++ hard/TanNetwork.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
 import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
@@ -9,6 +8,7 @@ import Data.Function
 import Data.Graph
 import Data.List
 import qualified Data.Map.Strict as Map
+import Data.Maybe
 import Data.STRef
 import Text.ParserCombinators.ReadP
 
@@ -31,9 +31,9 @@ data Stop =
 
 point :: ReadP Point
 point =
-  Point <$> radians <* comma <*> radians
+  Point <$> radians <*> radians
   where
-    radians = ((/ 180.0) . (* pi)) <$> readS_to_P reads
+    radians = ((/ 180.0) . (* pi)) <$> readS_to_P reads <* comma
 
 comma :: ReadP ()
 comma = void $ char ','
@@ -51,8 +51,8 @@ stop :: ReadP Stop
 stop =
   Stop
     <$> field
-    <*> (quote *> get `manyTill` quote) <* comma <* comma
-    <*> point <* replicateM 3 comma <* field
+    <*> ((quote *> get `manyTill` quote) <++ munch (',' /=)) <* comma <* field
+    <*> point <* replicateM 3 field <* munch (`notElem` "\r\n")
 
 list :: ReadP a -> ReadP [a]
 list item = do
@@ -61,7 +61,7 @@ list item = do
 
 edge :: ReadP (Ident,Ident)
 edge =
-  (,) <$> get `manyTill` (satisfy isSpace) <*> munch1 (not . isSpace)
+  (,) <$> get `manyTill` munch1 isSpace <*> munch1 (not . isSpace)
 
 request :: ReadP (Ident,Ident,[Stop],[(Ident,Ident)])
 request =
@@ -76,7 +76,7 @@ dist (Point lat1 lon1) (Point lat2 lon2) =
 main :: IO ()
 main = do
   input <- getContents
-  let [((src,tgt,stops,edges),"")] = readP_to_S request input
+  let ((src,tgt,stops,edges),""):_ = readP_to_S request (input ++ "\n")
   case solve src tgt stops edges of
     Nothing  -> putStrLn "IMPOSSIBLE"
     Just pth -> mapM_ (putStrLn . name) pth
@@ -85,6 +85,8 @@ infinity :: Double
 infinity = 1/0
 
 solve :: Ident -> Ident -> [Stop] -> [(Ident,Ident)] -> Maybe [Stop]
+solve src tgt stops edges
+  | src == tgt = Just [fromJust $ find ((src ==) . ident) stops]
 solve src tgt stops edges =
   let n2sArray  = listArray (1,length stops) stops
       -- number to stop
@@ -92,14 +94,18 @@ solve src tgt stops edges =
 
       i2nMap    = Map.fromList [ (ident s,n) | (n,s) <- assocs n2sArray ]
       -- ident to number
-      i2n i     = maybe (error "lookup failed") id $ i `Map.lookup` i2nMap
+      i2n i     = i `Map.lookup` i2nMap
 
       graph =
-        accumArray (flip (:)) [] (bounds n2sArray) $
-          map (liftA2 (,) (i2n . fst) (i2n . snd)) edges -- lift into ((->) r)
+        accumArray (flip (:)) [] (bounds n2sArray) $ do
+          (i1,i2) <- edges
+          let on1 = i2n i1
+              on2 = i2n i2
+          guard $ isJust on1 && isJust on2
+          return (fromJust on1,fromJust on2)
       n2p = position . n2s
       metric n1 n2 = dist (n2p n1) (n2p n2)
-  in  map n2s <$> astar graph metric (i2n src) (i2n tgt)
+  in  map n2s <$> astar graph metric (fromJust $ i2n src) (fromJust $ i2n tgt)
 
 type PQueue s v p = STRef s [(v,p)]
 
@@ -122,16 +128,16 @@ extractPQ queue = do
     hd:tl -> writeSTRef queue tl >> return (Just $ fst hd)
 
 astar :: Graph -> (Vertex -> Vertex -> Double) -> Vertex -> Vertex -> Maybe [Vertex]
-astar graph dist src tgt = runST $ do
+astar graph metric src tgt = runST $ do
   table <- newArray (bounds graph) (infinity,Nothing)
   queue <- newPQ
-  insertTPQ table queue src (dist src tgt) Nothing
+  insertTPQ table queue src 0 Nothing
   loop table queue
   where
     insertTPQ :: STArray s Vertex (Double,Maybe Vertex) -> PQueue s Vertex Double -> Vertex -> Double -> Maybe Vertex -> ST s ()
-    insertTPQ table queue nod est pre = do
-      writeArray table nod (est,pre)
-      insertPQ queue nod est
+    insertTPQ table queue nod dst pre = do
+      writeArray table nod (dst,pre)
+      insertPQ queue nod (dst + metric nod tgt)
     path :: STArray s Vertex (Double,Maybe Vertex) -> [Vertex] -> ST s [Vertex]
     path table pth@(cur:_) = do
       (_,opre) <- readArray table cur
@@ -151,9 +157,9 @@ astar graph dist src tgt = runST $ do
     expand :: STArray s Vertex (Double,Maybe Vertex) -> PQueue s Vertex Double -> Vertex -> ST s ()
     expand table queue cur =
       forM_ (graph ! cur) $ \nxt -> do
-        (curEst,_) <- readArray table cur
-        let newNxtEst = curEst - dist cur tgt + dist cur nxt + dist nxt tgt
-        (oldNxtEst,_) <- readArray table nxt
-        when (newNxtEst < oldNxtEst) $ do
+        (curDst,_) <- readArray table cur
+        let newNxtDst = curDst + metric cur nxt
+        (oldNxtDst,_) <- readArray table nxt
+        when (newNxtDst < oldNxtDst) $ do
           deletePQ queue nxt
-          insertTPQ table queue nxt newNxtEst (Just cur)
+          insertTPQ table queue nxt newNxtDst (Just cur)
